025af2d9f7f72aa7ebbe79960e9a4e90540f41c1
abebawwondie46-code <abebawwondie46@gmail.com>
2026-09-17 16:31:34 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'import 'dart:io';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:video_player/video_player.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package0:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'profile_screen.dart';
 import 'download_service.dart';
+import 'downloads_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
