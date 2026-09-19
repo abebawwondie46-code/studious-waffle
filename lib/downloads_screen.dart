e72bbcb63c9d0877c118ad389c3b77cd9ca2ed6b
abebawwondie46-code <abebawwondie46@gmail.com>
2026-09-19 09:57:47 +0300

    Update downloads_screen.dart

--- lib/downloads_screen.dart
+++ lib/downloads_screen.dart
@@ -3,9 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'download_service.dart';
-import 'package:dio/dio.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class DownloadsScreen extends StatefulWidget {
   const DownloadsScreen({super.key});
