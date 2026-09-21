83d601d55ed395edcce965cb69a03337b0cc92a5
abebawwondie46-code <abebawwondie46@gmail.com>
2026-09-21 08:51:46 +0300

    Update watch_party_screen.dart

--- lib/watch_party_screen.dart
+++ lib/watch_party_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class WatchPartyScreen extends StatefulWidget {
   const WatchPartyScreen({Key? key}) : super(key: key);
