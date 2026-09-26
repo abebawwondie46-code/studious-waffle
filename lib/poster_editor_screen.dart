e4139f9af8af0f2e8ab7bd22c59d5b88c0cf80e7
abebawwondie46-code <abebawwondie46@gmail.com>
2026-09-27 00:53:58 +0300

    Update poster_editor_screen.dart

--- lib/poster_editor_screen.dart
+++ lib/poster_editor_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:ui' as ui;
+import 'video_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
